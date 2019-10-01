7f38e8d3972bd88f9c39996b156be4adbb478831
steffy-lo <steffy-lo@hotmail.com>
2019-10-01 12:02:20 -0400

    Update sr_router.c

--- router/sr_router.c
+++ router/sr_router.c
@@ -14,7 +14,6 @@
 #include <stdio.h>
 #include <assert.h>
 
-
 #include "sr_if.h"
 #include "sr_rt.h"
 #include "sr_router.h"
@@ -30,7 +29,7 @@
  *
  *---------------------------------------------------------------------*/
 
-void sr_init(struct sr_instance* sr)
+void sr_init(struct sr_instance *sr)
 {
     /* REQUIRES */
     assert(sr);
@@ -45,7 +44,7 @@ void sr_init(struct sr_instance* sr)
     pthread_t thread;
 
     pthread_create(&thread, &(sr->attr), sr_arpcache_timeout, sr);
-    
+
     /* Add initialization code here! */
 
 } /* -- sr_init -- */
@@ -66,43 +65,46 @@ void sr_init(struct sr_instance* sr)
  *
  *---------------------------------------------------------------------*/
 
-void sr_handlepacket(struct sr_instance* sr,
-        uint8_t * packet/* lent */,
-        unsigned int len,
-        char* interface/* lent */)
+void sr_handlepacket(struct sr_instance *sr,
+                     uint8_t *packet /* lent */,
+                     unsigned int len,
+                     char *interface /* lent */)
 {
-  /* REQUIRES */
-  assert(sr);
-  assert(packet);
-  assert(interface);
+    /* REQUIRES */
+    assert(sr);
+    assert(packet);
+    assert(interface);
 
-  printf("*** -> Received packet of length %d \n",len);
+    printf("*** -> Received packet of length %d \n", len);
 
-  /* fill in code here */
-
-    if (len < sizeof(sr_ethernet_hdr_t)) {
+    if (len < sizeof(sr_ethernet_hdr_t))
+    {
         fprintf(stderr, "sr_handlepacket: Ethernet packet doesn't meet minimum length.\n");
         return;
     }
 
     /* It's an ARP Packet type*/
-    if (ethertype(packet) == ethertype_arp) {
-        if (len < sizeof(sr_ethernet_hdr_t) + sizeof(sr_arp_hdr_t)) {
+    if (ethertype(packet) == ethertype_arp)
+    {
+        if (len < sizeof(sr_ethernet_hdr_t) + sizeof(sr_arp_hdr_t))
+        {
             fprintf(stderr, "sr_handlepacket: ARP packet doesn't meet minimum length.\n");
             return;
         }
 
         /* ARP header is after the Ethernet header */
-        sr_arp_hdr_t* arp_hdr = (sr_arp_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t));
+        sr_arp_hdr_t *arp_hdr = (sr_arp_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t));
 
         /* Check hardware format code */
-        if(ntohs(arp_hdr->ar_hrd) != arp_hrd_ethernet) {
+        if (ntohs(arp_hdr->ar_hrd) != arp_hrd_ethernet)
+        {
             fprintf(stderr, "sr_handlepacket: unknown hardware address format.\n");
             return;
         }
 
         /* Check if protocol type is valid */
-        if(ntohs(arp_hdr->ar_pro) != ethertype_ip) {
+        if (ntohs(arp_hdr->ar_pro) != ethertype_ip)
+        {
             fprintf(stderr, "sr_handlepacket: invalid protocol type.\n");
             return;
         }
@@ -112,9 +114,11 @@ void sr_handlepacket(struct sr_instance* sr,
          * For ARP Replies: Cache the entry if the target IP address is one of your router’s IP addresses.
          * Check if target IP is one of router's IP addresses.
          * */
-        struct sr_if* if_walker = sr->if_list;
-        while(if_walker->next) {
-            if (if_walker->ip == arp_hdr->ar_tip) {
+        struct sr_if *if_walker = sr->if_list;
+        while (if_walker->next)
+        {
+            if (if_walker->ip == arp_hdr->ar_tip)
+            {
                 handle_arp(sr, arp_hdr, if_walker);
                 return;
             }
@@ -123,75 +127,170 @@ void sr_handlepacket(struct sr_instance* sr,
         fprintf(stderr, "sr_handlepacket: target IP cannot be found.\n");
     }
     /* It's a IP Packet type*/
-    else if (ethertype(packet) == ethertype_ip) {
+    else if (ethertype(packet) == ethertype_ip)
+    {
         /* Handle IP Packet */
 
-    }
-    else {
-        /* Drop the packet */
-        printf("Drop the packet.\n")
-    }
+        /* IP header is after the Ethernet header */
+        sr_ip_hdr_t *ip_hdr = (sr_arp_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t));
 
-}/* end sr_ForwardPacket */
-
-void handle_arp(struct sr_instance* sr, sr_arp_hdr_t* arp_hdr, struct sr_if* inf) {
-    switch(ntohs(arp_hdr->ar_op)) {
-        case arp_op_request: {
-            printf("Received an ARP request\n");
-
-            /* Construct ARP reply */
-            unsigned int len = sizeof(sr_ethernet_hdr_t);
-            uint8_t* arp_reply = malloc(len);
-
-            /* Set Ethernet Header */
-            sr_ethernet_hdr_t* reply_eth_hdr = (sr_ethernet_hdr_t*)arp_reply;
-            /* set destination MAC to be source MAC */
-            memcpy(reply_eth_hdr->ether_dhost, reply_eth_hdr->ether_shost, ETHER_ADDR_LEN);
-            /* set source MAC to be interface's MAC */
-            memcpy(reply_eth_hdr->ether_shost, inf->addr, ETHER_ADDR_LEN);
-
-            /* Set ARP Header */
-            sr_arp_hdr_t* reply_arp_hdr = (sr_arp_hdr_t*)(arp_reply + sizeof(sr_ethernet_hdr_t));
-            reply_arp_hdr->ar_hrd = arp_hdr->ar_hrd;
-            reply_arp_hdr->ar_pro = arp_hdr->ar_pro;
-            reply_arp_hdr->ar_hln = arp_hdr->ar_hln;
-            reply_arp_hdr->ar_pln = arp_hdr->ar_pln;
-            reply_arp_hdr->ar_op = htons(arp_op_reply);
-            /* set sender MAC to be interface's MAC */
-            memcpy(reply_arp_hdr->ar_sha, inf->addr, ETHER_ADDR_LEN);
-            /* set sender IP to be interface's IP */
-            reply_arp_hdr->ar_sip = inf->ip;
-            /* set target MAC to be the packet's sender MAC */
-            memcpy(reply_arp_hdr->ar_tha, arp_hdr->ar_sha, ETHER_ADDR_LEN);
-            /* set target IP to be the packet's sender IP */
-            reply_arp_hdr->ar_tip = arp_hdr->ar_sip;
-
-            printf("Send ARP reply.\n");
-            sr_send_packet(sr, arp_reply, len, inf->name);
-            free(arp_reply);
-            break;
-        }
-        case arp_op_reply: {
-            printf("Received an ARP reply.\n");
-            /* Look up request queue */
-            struct sr_arpreq* queued = sr_arpcache_insert(&sr->cache, arp_hdr->ar_sha, arp_hdr->ar_sip);
-            if(queued) {
-                struct sr_packet* queued_pkts = cached->packets;
-                /* Send outstanding packets */
-                while(queued_pkts) {
-                    struct sr_if* inf = sr_get_interface(sr, queued_pkts->iface);
-                    if(inf) {
-                        sr_ethernet_hdr_t* eth_hdr = (sr_ethernet_hdr_t*)(queued_pkts->buf);
-                        memcpy(eth_hdr->ether_dhost, arp_hdr->ar_sha, ETHER_ADDR_LEN);
-                        memcpy(eth_hdr->ether_shost, inf->addr, ETHER_ADDR_LEN);
-                        sr_send_packet(sr, queued_pkts->buf, queued_pkts->len, queued_pkts->iface);
-                    }
-                    queued_pkts = queued_pkts->next;
-                }
-                sr_arpreq_destroy(&sr->cache, cached);
+        /* Check if the IP address matches the current router's IP addresses */
+        struct sr_if *if_walker = sr->if_list;
+        while (if_walker->next)
+        {
+            if (if_walker->ip == ip_hdr->ip_dst)
+            {
+				handle_ip(sr, ip_hdr, if_walker)
+                return;
             }
-            break;
+            if_walker = if_walker->next;
         }
+        forward_ip(sr, ip_hdr);
+    }
+    else
+    {
+        /* Drop the packet */
+        printf("Drop the packet.\n");
+    }
+
+} /* end sr_ForwardPacket */
+
+void handle_arp(struct sr_instance *sr, sr_arp_hdr_t *arp_hdr, struct sr_if *inf)
+{
+    switch (ntohs(arp_hdr->ar_op))
+    {
+		case arp_op_request:
+		{
+			printf("Received an ARP request\n");
+			/* Construct ARP reply */
+			unsigned int len = sizeof(sr_ethernet_hdr_t);
+			uint8_t *arp_reply = malloc(len);
+
+			/* Set Ethernet Header */
+			sr_ethernet_hdr_t *reply_eth_hdr = (sr_ethernet_hdr_t *)arp_reply;
+			/* set destination MAC to be source MAC */
+			memcpy(reply_eth_hdr->ether_dhost, reply_eth_hdr->ether_shost, ETHER_ADDR_LEN);
+			/* set source MAC to be interface's MAC */
+			memcpy(reply_eth_hdr->ether_shost, inf->addr, ETHER_ADDR_LEN);
+
+			/* Set ARP Header */
+			sr_arp_hdr_t *reply_arp_hdr = (sr_arp_hdr_t *)(arp_reply + sizeof(sr_ethernet_hdr_t));
+			reply_arp_hdr->ar_hrd = arp_hdr->ar_hrd;
+			reply_arp_hdr->ar_pro = arp_hdr->ar_pro;
+			reply_arp_hdr->ar_hln = arp_hdr->ar_hln;
+			reply_arp_hdr->ar_pln = arp_hdr->ar_pln;
+			reply_arp_hdr->ar_op = htons(arp_op_reply);
+			/* set sender MAC to be interface's MAC */
+			memcpy(reply_arp_hdr->ar_sha, inf->addr, ETHER_ADDR_LEN);
+			/* set sender IP to be interface's IP */
+			reply_arp_hdr->ar_sip = inf->ip;
+			/* set target MAC to be the packet's sender MAC */
+			memcpy(reply_arp_hdr->ar_tha, arp_hdr->ar_sha, ETHER_ADDR_LEN);
+			/* set target IP to be the packet's sender IP */
+			reply_arp_hdr->ar_tip = arp_hdr->ar_sip;
+
+			printf("Send ARP reply.\n");
+			sr_send_packet(sr, arp_reply, len, inf->name);
+			free(arp_reply);
+			break;
+		}
+		case arp_op_reply:
+		{
+			printf("Received an ARP reply.\n");
+			/* Look up request queue */
+			struct sr_arpreq *queued = sr_arpcache_insert(&sr->cache, arp_hdr->ar_sha, arp_hdr->ar_sip);
+			if (queued)
+			{
+				struct sr_packet *queued_pkts = queued->packets;
+				/* Send outstanding packets */
+				while (queued_pkts)
+				{
+					struct sr_if *inf = sr_get_interface(sr, queued_pkts->iface);
+					if (inf)
+					{
+						sr_ethernet_hdr_t *eth_hdr = (sr_ethernet_hdr_t *)(queued_pkts->buf);
+						memcpy(eth_hdr->ether_dhost, arp_hdr->ar_sha, ETHER_ADDR_LEN);
+						memcpy(eth_hdr->ether_shost, inf->addr, ETHER_ADDR_LEN);
+						sr_send_packet(sr, queued_pkts->buf, queued_pkts->len, queued_pkts->iface);
+					}
+					queued_pkts = queued_pkts->next;
+				}
+				sr_arpreq_destroy(&sr->cache, queued);
+			}
+			break;
+		}
     }
 }
+void handle_ip(struct sr_instance *sr, sr_ip_hdr_t *ip_hdr, struct sr_if *inf)
+{
+    if (ip_hdr->ip_p == ip_protocol_icmp) {
+        printf("An ICMP message.\n");
 
+		/* ICMP header is after the IP header */
+        sr_icmp_hdr_t* icmp_hdr = (sr_icmp_hdr_t*)(ip_hdr + sizeof(sr_ip_hdr_t));
+
+        /* if it's an ICMP echo request, send echo reply */
+        if(icmp_hdr->icmp_type == icmp_type_echo_request) {
+
+        }
+
+    } else {
+        printf("A TCP/UDP message.\n");
+        /* Send ICMP type 3 code 3: Port Unreachable */
+    }
+    
+}
+
+void forward_ip(struct sr_instance *sr, sr_ip_hdr_t *ip_hdr)
+{
+	/* Update TTL */
+    ip_hdr->ip_ttl--;
+    if(ip_hdr->ip_ttl == 0) {
+        send_icmp_msg(sr, packet, len, icmp_type_time_exceeded, (uint8_t)0);
+        return;
+    }
+
+    /* Check the routing table and compare the values to the destination IP address */
+    struct sr_rt *cur_node = sr->routing_table;
+    int matching_mask = 0;
+    struct in_addr *matching_address;
+
+    while (cur_node->next)
+    {
+        /* Compare the packet destination and the destination in the routing table node, record how many bits match */
+        check_longest_prefix(cur_node, ip_hdr->ip_dst, &matching_mask, matching_address);
+    }
+    if (matching_address)
+    {
+        /* Check the ARP cache, and handle that */
+    }
+    /* If we get here, then matching_address was null, then we drop the packet and send an error */
+}
+
+void check_longest_prefix(struct sr_rt *cur_node, struct in_addr packet_dest, int *matching_mask, struct in_addr *matching_address)
+{
+    /* Mask the packet's destination address to get the prefix */
+    int masked_dest = packet_dest.s_addr & cur_node->mask.s_addr;
+    /* If the prefix matches the entry's destination as well, it's a match */
+    if (masked_dest == cur_node->dest.s_addr & cur_node->mask.s_addr)
+    {
+        /* If this is true then we know that this match is our best match (since the number of bits compared was higher)
+         Save the data for comparison later */
+        if (cur_node->mask.s_addr > *matching_mask)
+        {
+            *matching_mask = cur_node->mask.s_addr;
+            *matching_address = cur_node->dest;
+        }
+        /* If it's false then it's not our best match, just ignore it */
+    }
+    /* If the prefix doesn't match then we do nothing */
+}
+
+/* If the destinatino IP address doesn't match the router, then forward it
+ Take the destination IP address, compare it to the routing table (longest prefix matching)
+ If it matches some pair in the routing table
+ Use the ARP cache to see if we can find the MAC address of the target IP
+ If we can, we send out the packet using the target MAC address
+ If we can't then we broadcast an ARP request, and do the steps above after receiving a reply (or an error if no replies after 5 attempts)
+ If it doesn't match some pair in the routing table
+ Send an error back to the client */
